24db1227a07bf97fc6b440de2eeeb05ea18281a3
Jsumme1 <88805050+Jsumme1@users.noreply.github.com>
2022-11-03 16:11:22 -0500

    api routes and controllers

--- server.js
+++ server.js
@@ -35,6 +35,7 @@ app.get('/', (req, res) => {
   res.json({ message: 'Welcome to julie application' });
 });
 
+require('./app/routes/tutorial.routes')(app);
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
